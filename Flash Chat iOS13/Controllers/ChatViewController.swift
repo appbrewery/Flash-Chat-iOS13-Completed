f88384b6dde2218ac38f7b830589bc9af46fc3a3
vnaveen-coder <62545199+vnaveen-coder@users.noreply.github.com>
2020-04-20 08:24:55 +0530

    add messageTextfield shouldn't be empty.

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -62,20 +62,20 @@ class ChatViewController: UIViewController {
     
     @IBAction func sendPressed(_ sender: UIButton) {
         
-        if let messageBody = messageTextfield.text, let messageSender = Auth.auth().currentUser?.email {
-            db.collection(K.FStore.collectionName).addDocument(data: [
-                K.FStore.senderField: messageSender,
-                K.FStore.bodyField: messageBody,
-                K.FStore.dateField: Date().timeIntervalSince1970
-            ]) { (error) in
-                if let e = error {
-                    print("There was an issue saving data to firestore, \(e)")
-                } else {
-                    print("Successfully saved data.")
-                    
-                    DispatchQueue.main.async {
-                         self.messageTextfield.text = ""
-                    }
+        
+        if messageTextfield.text != ""{
+            if let messageBody = messageTextfield.text,let messageSender = Auth.auth().currentUser?.email
+            {
+                db.collection(K.FStore.collectionName).addDocument(data: [
+                    K.FStore.senderField : messageSender,
+                    K.FStore.dateField : Date().timeIntervalSince1970,
+                    K.FStore.bodyField : messageBody]) { (error) in
+                        if let e=error{
+                            print("issur saving data in firestore\(e)")
+                        }else{
+                            self.messageTextfield.text=""
+                            print("succesfull saved data")
+                        }
                 }
             }
         }
